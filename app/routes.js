1439a0a95d56d80d08054d24500c9932c0fd988d
wicoo <william.cooper@nhs.net>
2023-03-10 09:55:25 +0000

    update pages

--- app/routes.js
+++ app/routes.js
@@ -3,21 +3,46 @@ const express = require('express');
 
 const router = express.Router();
 
-// Branching example 1
+// Branching example 1 - Radios
 
-// this part where you form action go
-router.post('/example1/answer', (req, res) => {
-  // Make a variable and give it the value from 'know-nhs-number'
+// this is where your form action goes
+router.post('/example1/radios', (req, res) => {
+  // Make a variable and give it the value from 'know-nhs-number', you need a unique variable for each route
   const nhsNumber = req.session.data['know-nhs-number']; // This is the name attribute from your input
 
   // Check whether the variable matches a condition
   if (nhsNumber === 'Yes') {
     // Send user to next page
-    res.redirect('/example1/page1-yes-result');
+    res.redirect('/example1/radios-yes-result');
   } else {
     // Send user to ineligible page
-    res.redirect('/example1/page1-no-result');
+    res.redirect('/example1/radios-no-result');
   }
 });
+
+
+// Branching example 2 - Radios
+
+
+// Branching example 3 - Select 
+
+
+// Branching example 4 - Checkboxes 
+
+// this is where your form action goes
+router.post('/example1/checkbox', (req, res) => {
+  // Make a variable and give it the value from 'know-nhs-number'
+  const contactCheckbox = req.session.data['know-nhs-number']; // This is the name attribute from your input
+
+  // Check whether the variable matches a condition
+  if (contactCheckbox === 'Yes') {
+    // Send user to next page
+    res.redirect('/example2/checkbox-1-result');
+  } else {
+    // Send user to ineligible page
+    res.redirect('/example2/checkbox-2-result');
+  }
+});
+
 
 module.exports = router; // All your routes need to be above this line 
