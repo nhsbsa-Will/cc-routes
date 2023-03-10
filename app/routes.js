c4e6df4c2ea05aa0ac54db00f765055e6182bb98
wicoo <william.cooper@nhs.net>
2023-03-10 12:41:59 +0000

    update

--- app/routes.js
+++ app/routes.js
@@ -20,29 +20,43 @@ router.post('/example1/radios', (req, res) => {
   }
 });
 
-
-// Branching example 2 - Radios
-
-
-// Branching example 3 - Select 
+// EXAMPLE 2 - RADIOS
 
 
-// Branching example 4 - Checkboxes 
+// EXAMPLE 3 - TEXT INPUT
+router.post('/example3/name', function (req, res) {
+  var firstname = req.session.data['firstname'];
+  console.log(firstname);
+  var lastname = req.session.data['lastname'];
+  console.log(lastname);
 
-// this is where your form action goes
-router.post('/example1/checkbox', (req, res) => {
-  // Make a variable and give it the value from 'know-nhs-number'
-  const contactCheckbox = req.session.data['know-nhs-number']; // This is the name attribute from your input
-
-  // Check whether the variable matches a condition
-  if (contactCheckbox === 'Yes') {
-    // Send user to next page
-    res.redirect('/example2/checkbox-1-result');
-  } else {
-    // Send user to ineligible page
-    res.redirect('/example2/checkbox-2-result');
+  if (firstname && lastname) {
+    res.redirect('/example3/entered-name');
   }
+  else {
+    res.redirect('/example3/not-full-name');
+  }  
 });
 
+
+// EXAMPLE 4 - Checkboxes
+router.post('/example4/checkbox', (req, res) => {
+
+  // This creates an array of the values checked from the checkbox options
+  const contact = req.session.data['contact'];
+
+  if (contact == 'email') {
+    res.redirect('/example4/email-result'); // Send user to email result
+  } else if (contact == 'phone') {
+    res.redirect('/example4/phone-result'); // Send user to phone result
+  } else if (contact == 'text') {
+    res.redirect('/example4/textmsg-result'); // Send user to text result
+  } else if (contact.includes('email', 'phone', 'text')) {
+    res.redirect('/example4/email-phone-text-result'); // All items are checked
+  } else {
+    // Send user to ineligible page
+    res.redirect('/');
+  }
+}); 
 
 module.exports = router; // All your routes need to be above this line 
